e2c38fe929854c35786ef4ae93e4b9300b99b738
chrisbyd <chenyongbiao0319@sina.com>
2021-07-15 09:34:04 +0800

    modifying train.sh

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
 CUDA_VISIBLE_DEVICES=0 python3 tools/train_net.py --cfg configs/c_w_xbm.yaml
-CUDA_VISIBLE_DEVICES=0 python3 tools/train_net.py --cfg configs/c_wo_xbm.yaml  
+#CUDA_VISIBLE_DEVICES=0 python3 tools/train_net.py --cfg configs/c_wo_xbm.yaml  
